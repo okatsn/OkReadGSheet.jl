b2eebfa92326fe2bdacab54369cd75358105cc7e
Documenter.jl <documenter@juliadocs.github.io>
2024-05-12 08:18:49 +0000

    build based on 1162f65

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OkReadGSheet","category":"page"},{"location":"#OkReadGSheet","page":"Home","title":"OkReadGSheet","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkReadGSheet.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OkReadGSheet]","category":"page"}]
+}
