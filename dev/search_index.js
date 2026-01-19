64c3d737598367a1e16f7eac3dffaa9a164cb24d
Documenter.jl <documenter@juliadocs.github.io>
2026-01-19 07:17:04 +0000

    build based on b83bfdc

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#OkReadGSheet","page":"Home","title":"OkReadGSheet","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkReadGSheet.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"#OkReadGSheet.readgsheet-Tuple{Any}","page":"Home","title":"OkReadGSheet.readgsheet","text":"readgsheet(url; kwargs...)\n\n\n\n\n\n","category":"method"}]
+[{"location":"#OkReadGSheet","page":"Home","title":"OkReadGSheet","text":"Documentation for OkReadGSheet.\n\n","category":"section"},{"location":"#OkReadGSheet.readgsheet-Tuple{Any}","page":"Home","title":"OkReadGSheet.readgsheet","text":"readgsheet(url; kwargs...)\n\n\n\n\n\n","category":"method"}]
 }
